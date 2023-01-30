cc16feb14ceb5ed32f54f0168aeb5952048b77fd
eunchae-cho <dmsco8981@gmail.com>
2023-01-31 00:02:44 +0900

    ,

--- task/src/main/resources/static/js/video.js
+++
@@ -1,4 +0,0 @@
-console.log('test12')
-const inputFile = document.querySelector('#input-file');
-const video = document.querySelector('#video');
-const videoSource = document.querySelector('#video-source');
